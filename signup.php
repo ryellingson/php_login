6c4356286df5ac64cc4db5dc6c684ab0fc7cfccb
Ry Ellingson <rycellingson@gmail.com>
2022-02-26 17:34:04 -1000

    further php and database creation

--- signup.php
+++ signup.php
@@ -3,11 +3,9 @@
         <form class="login-form" action="signup.inc.php" method="post">
             <img class="lock" src="lock.png" alt="lock">
             <input type="text" name="name" placeholder="username">
-            <input type="text" name="email" placeholder="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
             <input type="password" name="pwd" placeholder="password" required />
             <div class="buttons">
                 <button type="submit">Sign Up</button>
-                <button type="submit">Log In</button>
             </div>
         </form>  
     </div>
