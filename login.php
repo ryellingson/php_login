705fec3b76448e95e3688f18ae731ce30541bab4
Ry Ellingson <rycellingson@gmail.com>
2022-02-27 11:51:57 -1000

    signup scripts

--- login.php
+++ login.php
@@ -1,15 +1,13 @@
-<style>
-<?php include_once 'style.css'; ?>
-</style>
+<style><?php include_once 'style.css'; ?></style>
 
 <div class="wrapper">
     <div class="form">
-        <form class="login-form" action="signup.inc.php" method="post">
+        <form class="login-form" action="includes/signup.inc.php" method="post">
             <img class="lock" src="lock.png" alt="lock">
             <input type="text" name="name" placeholder="username">
             <input type="password" name="pwd" placeholder="password" required />
             <div class="buttons">
-                <button type="submit">Log In</button>
+                <button type="submit" name="submit">Log In</button>
             </div>
         </form>  
     </div>
--- signup.php
+++ signup.php
@@ -1,15 +1,14 @@
-<style>
-<?php include_once 'style.css'; ?>
-</style>
+<style><?php include_once 'style.css'; ?></style>
 
 <div class="wrapper">
     <div class="form">
-        <form class="login-form" action="signup.inc.php" method="post">
+        <form class="login-form" action="includes/signup.inc.php" method="post">
             <img class="lock" src="lock.png" alt="lock">
             <input type="text" name="name" placeholder="username">
             <input type="password" name="pwd" placeholder="password" required />
+            <input type="password" name="pwdrepeat" placeholder="repeat password" required />
             <div class="buttons">
-                <button type="submit">Sign Up</button>
+                <button type="submit" name="submit">Sign Up</button>
             </div>
         </form>  
     </div>
