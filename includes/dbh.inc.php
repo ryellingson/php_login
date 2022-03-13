b7463a3097eaec034b95c125c689140391f403d6
Ry Ellingson <rycellingson@gmail.com>
2022-03-12 18:29:10 -0800

    heroku config

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,23 @@
 <?php
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsystem";
+// Used for local development
+// $serverName = "localhost";
+// $dBUsername = "root";
+// $dBPassword = "";
+// $dBName = "loginsystem";
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName); 
+// $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName); 
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error() );
