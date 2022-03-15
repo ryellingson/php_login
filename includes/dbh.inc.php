38f78aeb4146e2dca5bee56a4cd27e56b22ddae3
Ry Ellingson <rycellingson@gmail.com>
2022-03-15 13:24:02 -0700

    more error handling

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -12,15 +12,25 @@ error_reporting(E_ALL);
 // $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName); 
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+try {
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"],1);
+  $active_group = 'default';
+  $query_builder = TRUE;
+  // Connect to DB
+  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+} catch(\Exception $e) {
+	echo "Exception: " . $e->getMessage();
+}
+
+$loggableConfigVariable = $config;
+
+unset($loggableConfigVariable['pass']);
+
+file_put_contents("config.deletemewhendone.log", json_encode($loggableConfigVariable, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL);
 
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error() );
